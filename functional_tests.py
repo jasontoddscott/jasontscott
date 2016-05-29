4c6bfe10a97c8fa276fa4593ff4e8776e087dd16
Jason Scott <jasontoddscott@gmail.com>
2016-05-28 20:30:09 -0700

    Added basic view and tests.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -10,7 +11,8 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_blog_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.implicitly_wait(9)
+        self.browser.get('http://127.0.0.1:8000')
 
         self.assertIn('Blog', self.browser.title)
         self.fail('Finish the test!')
